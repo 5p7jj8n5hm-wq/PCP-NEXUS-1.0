4730c25065ba3273c082d6061a9bc63818ae81ed
5p7jj8n5hm-wq <5p7jj8n5hm@privaterelay.appleid.com>
2026-09-25 21:51:51 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='pcp-nexus-1.3.2';
+const C='pcp-nexus-1.5.1';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='pcp-nexus-1.3.2').map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
